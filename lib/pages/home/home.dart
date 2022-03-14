185f8457982016c23491895178c75dfb83b25404
Jorge Zapata <jorgejaden@gmail.com>
2022-03-13 21:57:36 -0500

    evening update

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -2,6 +2,7 @@ import 'package:dish_connect/controllers/navigation_controller.dart';
 import 'package:dish_connect/widgets/custom_text.dart';
 import 'package:dish_connect/routing/router.dart';
 import 'package:dish_connect/widgets/main_textfield.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,9 +16,16 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: CustomText(
-        text: "Home Page",
+      child: FlatButton(
+        child: Text("Press me"),
+        onPressed: () {
+          FirebaseCrashlytics.instance.crash();
+          print("hi");
+        },
       ),
+      // CustomText(
+      //   text: "Home Page",
+      // ),
     );
   }
 }
